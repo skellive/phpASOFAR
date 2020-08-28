ed1ee85a045d63ca165b6c31d2ff6b1def9b5caf
galojose38 <galojose38@gmail.com>
2020-08-28 00:38:03 -0500

    Cambio

--- producto/insertar/insertarEnvaseProducto.php
+++ producto/insertar/insertarEnvaseProducto.php
@@ -1,19 +1,21 @@
 <?php
-require_once ('../../conexion.php');
-$mysql = new connection();
-$conexion = $mysql->get_connection();
-$envase='BOTELLA';
-//Consulta
-//@valor1 esta demas pero es necesario
-$sql='CALL insertarEnvaseProducto(?,@valor1)'; 
-$statement = $conexion->prepare($sql);
-//  i=int s=string d=decimal
-$statement->bind_param('s',$envase);
-$statement->execute();
-$statement->close();
-$conexion->close();
-
-
-
+//$_POST["envase"]="TUBO";
+if (!empty($_POST["envase"])) {
+    $envase = $_POST["envase"];
+    require_once('../../conexion.php');
+    $mysql = new connection();
+    $conexion = $mysql->get_connection();
+    //Consulta
+    //@valor1 esta demas pero es necesario
+    $sql = 'CALL insertarEnvaseProducto(?,@valor1)';
+    $statement = $conexion->prepare($sql);
+    //  i=int s=string d=decimal
+    $statement->bind_param('s', $envase);
+    $statement->execute();
+    $statement->close();
+    $conexion->close();
+} else {
+    echo('CAMPO VACIO');
+}
 
 ?>
