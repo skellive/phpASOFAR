22e6a0337bf2d768b057609708622738f3ff7e5a
galojose38 <galojose38@gmail.com>
2020-09-02 11:35:41 -0500

    pruebas

--- compra/insertar/PRUEBAS_NO_TOCAR.php
+++ compra/insertar/PRUEBAS_NO_TOCAR.php
@@ -1,3 +1,8 @@
-<?php  
+<?php
+require_once('../metodos/Metodos.php');
+$metodos = new Metodos();
+date_default_timezone_set("America/Guayaquil");
+$fechaHora = date('Y-m-d H:i:s');
+$fecha = date('Y-m-d');
 
 ?>
